a2fa373a2cc8e392ebafd4e975c53710c2f14028
SanjnaPanwar <sanjnapanwar7697@gmail.com>
2024-01-12 15:40:20 +0530

    fix

--- strapiTostrapiAssessment/Api.js
+++ strapiTostrapiAssessment/Api.js
@@ -4,7 +4,7 @@ const postBaseURL = "http://3.110.213.190/api";
 const tableName = "assessments";
 // let BEARER_TOKEN =
 //   "b0041255f0d636b62860aaa933fdc6d186d2a85652da44c81c2ea21a20140f3fcda7197713b23920a4a971a87b3bd694cff0429df90e604da040a9550f8a3f1de5d478700ca004debfc0344a5adda29854c6b717ddd579687c2212ffcf974b647b2e33352728257a1b1c63d893022cd03837c57dc82e49accb644f1d439824a4";
-const BEARER_TOKEN = '97194fcf58ac129f284485aede83cac7e662c5a50af07162c84c22575c392b60ebd3b3ffeb3afe7a7a8e26cc1d6f2b6943fe0c542f04dabb4febbf0069fcf02353462266c7192f7ba04bd3a3d3e07d256feee16f106501772a0078e57a49df6196922b936189bc6e1b74ce2bbaecfe8fbbfd0bcb145ace48e224f872fcf8d1c8'
+const BEARER_TOKEN = '280117da5dfa181ef74b08bd6f454e0f1ce1e050306a3717947130b67482a8a81220f8864bcecece303b6b336a2979ce3ec1ba1996adaa7baf59c0065860b37ba80d90241aef3ec9f51dfd0083b0f0d61554cec70d4b1c1cb8d9bc3e12a1431e3fdb1f21317f1fb8c961096141e7a28a6c23f65c9ea26b922104a1ca7843ab8d';
 const updateData = async (
   modifyQuestion,
   modifyOption,
@@ -35,7 +35,7 @@ const updateData = async (
     });
     
     
-    console.log('👉 Returned data:', res.data.data);
+    console.log('👉 Slug id:', res.data.data.id);
     const { data: { id } } = res.data;
     let slugId = id;
     data.data['slug']=slugId
@@ -48,7 +48,7 @@ const updateData = async (
       },
     });
 
-    console.log("Update successful", resp.data);
+    console.log("Update successful", resp.data.data.id);
     return resp;
   } catch (error) {
     console.log();
--- strapiTostrapiAssessment/fetchData.js
+++ strapiTostrapiAssessment/fetchData.js
@@ -15,7 +15,7 @@ const MerakiAccessData = async () => {
     const resp = await strapi.find("assessments");
     
     const totalAssessment = 591;
-    for (let id = 2; id <= totalAssessment; id++) {
+    for (let id = 1; id <= totalAssessment; id++) {
       // 863,
       const data = await strapi.findOne("assessments", id, {
         populate: ["dynamic", "explaination","exercise","course"],
